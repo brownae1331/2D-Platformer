d8edd0b8c582e33e08aee4ee0fb6b0dd19307cb4
brownae1331 <brownae1331@gmail.com>
2022-10-10 09:44:52 +0100

    Terrain
    Changed the tiles from a grey colour to look like grass blocks

---
+++ Code/powerup.py
@@ -0,0 +1,6 @@
+import pygame
+
+
+class PowerUp(pygame.sprtie.Sprite):
+    def __init__(self):
+        super().__init__()
--- Code/tiles.py
+++ Code/tiles.py
@@ -6,10 +6,10 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, size):  # The tile has to know its position on the screen and its size
         super().__init__()
         # The tile is a square so the width and height are the same
-        self.image = pygame.Surface((size, size))
+        self.image = pygame.image.load('Assests/Terrain/Grass Block.png')
+        self.image = pygame.transform.scale(self.image, (size, size))
         # Place the top left of the tile where the position is
         self.rect = self.image.get_rect(topleft=pos)
-        self.image.fill('grey')  # Not permanent colour
 
     # Keep the world shift to the x position of all tiles
     def update(self, xShift):
